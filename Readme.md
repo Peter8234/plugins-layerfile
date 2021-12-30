5351f95728c8ea02036e3a91dba6ab904dc6214a
Peter8234 <peter@webapp.io>
2021-12-30 07:24:46 -0500

    .gradle

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Full featured Layerfile Extensions for the Intellij Platforms
+
+Should work with every Intellij Platforms Plugin was tested using intellij Idea
+
+## Publishing
+
+## Functionality
+It has the following language features:
+- Semantic Tokens
+- Diagnostics for Layerfiles
+- Code Completion
+## Development Notes
+- Use intellij Idea for plugin development
+- To test the functionality of the plugin goto gradle -> runIde
+- For more information [link](https://plugins.jetbrains.com/docs/intellij/gradle-prerequisites.html)
--- src/main/java/org/webapp/sdk/language/LayerDocumentationProvider.java
+++ src/main/java/org/webapp/sdk/language/LayerDocumentationProvider.java
@@ -1,13 +1,16 @@
 package org.webapp.sdk.language;
 import com.intellij.lang.documentation.AbstractDocumentationProvider;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class LayerDocumentationProvider extends AbstractDocumentationProvider {
 
     @Override
-    public @Nullable String generateDoc(PsiElement element, @Nullable PsiElement originalElement) {
+    @Nullable
+    @Nls
+    public String generateDoc(PsiElement element, @Nullable PsiElement originalElement) {
         System.out.println("layer documentation printed");
         System.out.println(element);
         return "TEST STRING";
